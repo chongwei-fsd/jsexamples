a2d7fe306222afc0e51b7d9587ad32263aad5958
chongwei-fsd <ChongWei@genstudents.org>
2024-09-03 09:05:34 +0800

    updated index.js

--- k_assessment_structure_v3_skeleton/index.js
+++ k_assessment_structure_v3_skeleton/index.js
@@ -11,126 +11,133 @@ const RED = '\x1b[31m';
 const GREEN = '\x1b[32m';
 const BLUE = '\x1b[94m';
 const RESET = '\x1b[0m';
-const WIN = `${GREEN}🎉 👍 You found the carrot!${RESET}`;  // TODO: customise message when player wins
-const LOST = `${RED}😭 You fell into a hole!${RESET}`;  // TODO: customise message when player lose
-const OUT = `${RED}😵 You have stepped out of the field!${RESET}`;  // TODO: customise message when player is out of bounds (lose)
+const WIN = `${GREEN}🎉 👍 You found the carrot!${RESET}`;
+const LOST = `${RED}😭 You fell into a hole!${RESET}`;
+const OUT = `${RED}😵 You have stepped out of the field!${RESET}`;
 const QUIT = `${GREEN}Thank you. You quit the game.${RESET}`;
 
-// const ROWS = 8;  // the game map will have 8 rows
-// const COLS = 5;  // the game map will have 5 cols
 let ROWS;
 let COLS;
-const PERCENTAGE = .2;  // % of holes for the map
+const PERCENTAGE = .2;
 
 class Field {
 
-    // TODO: create the constructor
-    constructor(field = [[]]) {  /* this.field is a property of the class Field */
-        this.field = field;
-        this.gamePlay = false;
-        this.playerMove = [0, 0]; //initialize row and column value 
+    constructor(field = [[]]) {
+        this._field = field;
+        this._gamePlay = false;
+        this._playerRow = 0;
+        this._playerCol = 0;
+        this._newRow = 0;
+        this._newCol = 0;
     }
 
-    static welcomeMsg(msg) {  // static Method to show game's welcome message
+    // Getters and setters for player position and game state
+    get playerRow() {
+        return this._playerRow;
+    }
+
+    set playerRow(value) {
+        this._playerRow=value
+    }
+
+    get playerCol() {
+        return this._playerCol;
+    }
+
+    set playerCol(value) {
+      this._playerCol=value
+    }
+
+    get gamePlay() {
+        return this._gamePlay;
+    }
+
+    set gamePlay(value) {
+        this._gamePlay = value;
+    }
+
+    get field() {
+        return this._field;
+    }
+
+    set field(value) {
+        this._field = value;
+    }
+
+    static welcomeMsg(msg) {
         console.log(msg);
     }
 
-    static generateField(rows, cols, percentage) {  // static method that generates and return a 2d map
+    static generateField(rows, cols, percentage) {
         const map = [[]];
         for (let i = 0; i < rows; i++) {
             map[i] = [];
             for (let j = 0; j < cols; j++) {
-                map[i][j] = Math.random() > percentage ? GRASS : HOLE;  //in each row, grass 80%, hole 20%
+                map[i][j] = Math.random() > percentage ? GRASS : HOLE;
             }
         }
         return map;
     }
 
-    printField() {  // print the game field (used to update during gameplay)       
-        // this.field.forEach((row)=>{
-        //     console.log(row.join('|')) //print field at the bottom of the screen
-        // })
+    printField() {
         console.table(this.field);
     }
 
-    updateGame(input) {  // TODO: Refer to details in the method's codeblock
+    updateGame(input) {
         const userInput = String(input.toLowerCase());
 
-        //r=newRow, c=newCol
-        let [r, c] = this.playerMove;
-
-        // move player based on input
+        // Move player based on input
         switch (userInput) {
-            case 'w': r -= 1; break;
-            case 's': r += 1; break;
-            case 'a': c -= 1; break;
-            case 'd': c += 1; break;
+            case 'w': this._newRow = this.playerRow - 1; break;
+            case 's': this._newRow = this.playerRow + 1; break;
+            case 'a': this._newCol = this.playerCol - 1; break;
+            case 'd': this._newCol = this.playerCol + 1; break;
             default:
         }
 
-        /*  
-        TODO: if the user exits out of the field
-        end the game - set the gamePlay = false;
-        inform the user that he step OUT of the game
-        */
-        if (r < 0 || c < 0 || r > ROWS - 1 || c > COLS - 1) { //ROWS=8, COLS=5 / index row=7, index col=4
+        // Check out-of-bounds
+        if (this._newRow < 0 || this._newCol < 0 || this._newRow >= ROWS || this._newCol >= COLS) {
             console.log(OUT);
             this.endGame();
             return;
         }
 
-        /*   
-        TODO: if the user arrives at the carrot
-        end the game - set gamePlay = false;
-        inform the user that he WIN the game 
-        */
-        if (this.field[r][c] === CARROT) {
+        // Check for carrot
+        if (this.field[this._newRow][this._newCol] === CARROT) {
             console.log(WIN);
             this.endGame();
             return;
         }
 
-        /* 
-        TODO: if the user arrives at the hole
-        end the game - set the gamePlay = false;
-        inform the user that he LOST the game
-        */
-        if (this.field[r][c] === HOLE) {
+        // Check for hole
+        if (this.field[this._newRow][this._newCol] === HOLE) {
             console.log(LOST);
             this.endGame();
             return;
         }
 
-        /*  
-        TODO: if the user ends the game
-        end the game - set the gamePlay = false;
-        inform the user that he QUIT the game
-        */
+        // Quit game
         if (userInput === 'q') {
             this.quitGame();
             return;
         }
 
-        /* 
-        TODO: otherwise, move player on the map: this.field[rowindex][colindex] = CARROT;
-        update this.field to show the user had moved to the new area on map
-        */
-        this.field[this.playerMove[0]][this.playerMove[1]] = GRASS; //current player position change to grass 
-        this.field[r][c] = PLAYER; //update new position(row,col) change it to player *
-        this.playerMove = [r, c]; //update new move position(row,col)value
-        // console.log(this.playerMove)  
+        // Move player on the map
+        this.field[this.playerRow][this.playerCol] = GRASS;
+        this.field[this._newRow][this._newCol] = PLAYER;
+        this.playerRow = this._newRow;
+        this.playerCol = this._newCol;
     }
 
     plantCarrot() {
-        const x = Math.floor(Math.random() * ROWS - 1) + 1;
-        const y = Math.floor(Math.random() * COLS - 1) + 1;
+        const x = Math.floor(Math.random() * ROWS);
+        const y = Math.floor(Math.random() * COLS);
         this.field[x][y] = CARROT;
     }
 
     startGame() {
         this.gamePlay = true;
         this.field[0][0] = PLAYER;
-
         this.plantCarrot();
 
         while (this.gamePlay) {
@@ -146,7 +153,7 @@ class Field {
                 case 'a': console.log(`${GREEN}Left${RESET}`); break;
                 case 'd': console.log(`${GREEN}Right${RESET}`); break;
                 case 'q': console.log(`${RED}Quit${RESET}`); break;
-                default: console.log(`${RED}Invalid key${RESET}`); flagInvalid = !flagInvalid;  // true
+                default: console.log(`${RED}Invalid key${RESET}`); flagInvalid = true;
             }
 
             if (!flagInvalid) {
@@ -156,7 +163,7 @@ class Field {
     }
 
     endGame() {
-        this.gamePlay = false;  // set property gamePlay to false
+        this.gamePlay = false;
         process.exit();
     }
 
@@ -168,17 +175,16 @@ class Field {
     static fieldSize(text) {
         let num;
         while (true) {
-            num = (prompt(text));
-            if (!isNaN(num) && num >= 5 && num <= 10 && num != " ") {  //isNaN(num) returns false, !isNaN(num) returns true
-                return num;
+            num = prompt(text);
+            if (!isNaN(num) && num >= 5 && num <= 10 && num.trim() !== "") {
+                return parseInt(num, 10);
             }
-            console.log(`${RED}Invalid! Input must be number (between 5-10) & no space!${RESET}`);
+            console.log(`${RED}Invalid! Input must be a number (between 5-10) & no space!${RESET}`);
         }
     }
-
 }
 
-// prompt user enter rows and cols (5-10) for field size
+// Prompt user to enter rows and cols (5-10) for field size
 ROWS = Field.fieldSize('Enter number of rows: ');
 COLS = Field.fieldSize('Enter number of cols: ');
 
